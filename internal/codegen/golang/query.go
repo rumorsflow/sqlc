81b001805bfcc0d5340b789e9bd467e97a9d0090
Igor Agapie <igoragapie@gmail.com>
2025-12-18 12:46:35 +0100

    Extend Go codegen to support `CmdSeq2` query type

--- internal/codegen/golang/query.go
+++ internal/codegen/golang/query.go
@@ -271,7 +271,8 @@ type Query struct {
 
 func (q Query) hasRetType() bool {
 	scanned := q.Cmd == metadata.CmdOne || q.Cmd == metadata.CmdMany ||
-		q.Cmd == metadata.CmdBatchMany || q.Cmd == metadata.CmdBatchOne
+		q.Cmd == metadata.CmdBatchMany || q.Cmd == metadata.CmdBatchOne ||
+		q.Cmd == metadata.CmdSeq2
 	return scanned && !q.Ret.isEmpty()
 }
 
--- internal/codegen/golang/result.go
+++ internal/codegen/golang/result.go
@@ -334,6 +334,7 @@ var cmdReturnsData = map[string]struct{}{
 	metadata.CmdBatchOne:  {},
 	metadata.CmdMany:      {},
 	metadata.CmdOne:       {},
+	metadata.CmdSeq2:      {},
 }
 
 func putOutColumns(query *plugin.Query) bool {
