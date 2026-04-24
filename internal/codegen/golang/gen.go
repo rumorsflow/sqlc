f2e17fcc60147b2ff14f1b9e2f9129a622219567
Igor Agapie <igor@thehouseofmouse.net>
2026-04-25 00:02:55 +0200

    Remove support for emitting result struct pointers in code generation

--- internal/codegen/golang/gen.go
+++ internal/codegen/golang/gen.go
@@ -34,7 +34,6 @@ type tmplCtx struct {
 	EmitPreparedQueries       bool
 	EmitInterface             bool
 	EmitEmptySlices           bool
-	EmitResultStructPointers  bool
 	EmitMethodsWithDBArgument bool
 	EmitEnumValidMethod       bool
 	EmitAllEnumValues         bool
@@ -179,7 +178,6 @@ func generate(req *plugin.GenerateRequest, options *opts.Options, enums []Enum, 
 		EmitDBTags:                options.EmitDbTags,
 		EmitPreparedQueries:       options.EmitPreparedQueries,
 		EmitEmptySlices:           options.EmitEmptySlices,
-		EmitResultStructPointers:  options.EmitResultStructPointers,
 		EmitMethodsWithDBArgument: options.EmitMethodsWithDbArgument,
 		EmitEnumValidMethod:       options.EmitEnumValidMethod,
 		EmitAllEnumValues:         options.EmitAllEnumValues,
